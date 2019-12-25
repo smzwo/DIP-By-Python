f2f772c2d5b7b9c024f24029a7e30f39ead08868
smz <v_sunmingzhe01@baidu.com>
2019-12-25 14:57:41 +0800

    project

---
+++ 工程文件夹，方便我在不同电脑上直接用工程/ch/ch03-1.py
@@ -0,0 +1,31 @@
+import cv2
+
+def myLineAdjust(image, k, b):
+    # 将tuple中的元素取出，赋值给height，width，channels
+    height = image.shape[0]
+    width = image.shape[1]
+    channels = image.shape[2]
+
+    for row in range(height):
+        for col in range(width):
+            # 若有多通道，则每个通道像素都会改变
+            for channel in range(channels):
+                image[row][col][channel] = 255 if image[row][col][channel] * k + b > 255 else image[row][col][channel] * k + b
+                # val = image[row][col][channel]
+                # newVal = val * k + b
+                # if newVal > 255 :
+                #     image[row][col][channel] = 255
+                # else:
+                #     image[row][col][channel] = newVal
+
+    return image
+
+if __name__ == '__main__':
+
+    image = cv2.imread('cat.jpg')
+    cv2.imshow('srcImage', image)
+
+    dstImage = myLineAdjust(image, 1, 100)
+    cv2.imshow('dstImage', dstImage)
+
+    cv2.waitKey(0)
