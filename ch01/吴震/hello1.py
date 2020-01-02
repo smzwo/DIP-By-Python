afa3dec8b6c7f1a42c5245cf5d531e3c190f8dfd
孙明喆 <sunmingzhe@sunmingzhedeMacBook-Pro.local>
2020-01-02 15:38:08 +0800

    修改目录结构

--- ch01/吴震/hello1.py
+++
@@ -1,9 +0,0 @@
-import cv2
-#读取文件
-img = cv2.imread("cat.jpg")
-#声明窗口（可省略）
-cv2.namedWindow("img")
-#显示图像
-cv2.imshow("img", img)
-#按键等待  0表示一直等待
-cv2.waitKey(0)
--- 工程文件夹，方便我在不同电脑上直接用工程/ch/ch01-1.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-# 此方法需要将相应的图片放置到工程目录下
-imgae = cv2.imread("cat.jpg")
-
-cv2.imshow("windowsName", imgae)
-
-cv2.waitKey(0)
